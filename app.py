8ed9ca899bd12b7f233ed5cde7306dc4643624be
Maxime Guinot <maxime.guinot@saagie.com>
2023-11-15 09:07:09 +0000

    update

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ s3_access_key = os.environ["AWS_ACCESS_KEY_ID"]
 s3_secret_key = os.environ["AWS_SECRET_ACCESS_KEY"]
 s3_bucket_name = 'template-projects'
 
-
 s3 = boto3.client("s3",
                   endpoint_url=os.environ["AWS_S3_ENDPOINT"],
                   region_name=os.environ["AWS_REGION_NAME"],
@@ -46,15 +45,36 @@ def update_file_list(n_clicks, path):
         # List objects in the specified path
         objects = s3.list_objects(Bucket=s3_bucket_name, Prefix=path)['Contents']
 
-        # Create a list of file information as text
+        # Create a list of file information as text and clickable links
         file_info = []
         for obj in objects:
-            file_info.append(f"File: {obj['Key']}, Size: {obj['Size']} bytes")
+            file_path = obj['Key']
+            file_size = obj['Size']
+            file_info.append(html.Div([
+                dcc.Link(f"File: {file_path}, Size: {file_size} bytes", href=f'/path/{file_path}')
+            ]))
 
     except NoCredentialsError:
         return "Credentials not available."
 
-    return [html.P(info) for info in file_info]
+    return file_info
+
+
+# Callback to handle drill-through links
+@app.callback(
+    Output('path-input', 'value'),
+    [Input('file-list', 'children')],
+    prevent_initial_call=True
+)
+def update_path_on_link_click(children):
+    # Get the path from the clicked link
+    clicked_link = [child for child in children if isinstance(child, html.Div) and 'dcc-link' in child.props['children'][0].props['className']]
+
+    if clicked_link:
+        path = clicked_link[0].props['children'][0].props['children'].split(": ")[1].split(",")[0]
+        return path
+    else:
+        raise dash.exceptions.PreventUpdate
 
 
 if __name__ == '__main__':
