2b43ea3d6a1151e821b4d13340aa625e3e8ec9b2
Maxime Guinot <maxime.guinot@saagie.com>
2023-11-16 20:15:22 +0000

    test

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ app = dash.Dash(__name__, external_stylesheets=[themes.BOOTSTRAP])
 # AWS S3 credentials
 s3_access_key = os.environ["AWS_ACCESS_KEY_ID"]
 s3_secret_key = os.environ["AWS_SECRET_ACCESS_KEY"]
-s3_bucket_name = 'template-projects'
 
 s3 = boto3.client("s3",
                   endpoint_url=os.environ["AWS_S3_ENDPOINT"],
@@ -20,18 +19,16 @@ s3 = boto3.client("s3",
                   aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
                   aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"])
 
-# Function to format file sizes
-def format_file_size(size):
-    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
-        if size < 1024.0:
-            break
-        size /= 1024.0
-    return "{:.2f} {}".format(size, unit)
-
 # Dash layout
 app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),  # Add Location component
     html.H1("S3 Browser"),
+    dcc.Dropdown(
+        id='bucket-dropdown',
+        options=[],
+        multi=False,
+        value='',
+        placeholder='Select a bucket'
+    ),
     dcc.Loading(
         id="loading",
         type="default",
@@ -41,42 +38,63 @@ app.layout = html.Div([
     ),
 ])
 
-# Callback to update file list based on input path
+# Callback to update bucket dropdown options
 @app.callback(
-    Output('file-list', 'children'),
-    [Input('url', 'pathname')],
+    Output('bucket-dropdown', 'options'),
+    [Input('bucket-dropdown', 'search_value')],
     prevent_initial_call=True
 )
-def update_file_list(pathname):
+def update_bucket_options(search_value):
     try:
-        # Extract the folder path from the URL
-        path = pathname.split("/path/")[-1] if "/path/" in pathname else ""
+        # List all available buckets
+        buckets = s3.list_buckets()['Buckets']
 
-        # If the path is empty, set it to the root level
-        path = path or ""
+        # Create options for the dropdown
+        options = [{'label': bucket['Name'], 'value': bucket['Name']} for bucket in buckets]
 
-        # List objects in the specified path
-        objects = s3.list_objects(Bucket=s3_bucket_name, Prefix=path)['Contents']
+    except NoCredentialsError:
+        return []
+
+    return options
+
+# Callback to update file list based on selected bucket
+@app.callback(
+    Output('file-list', 'children'),
+    [Input('bucket-dropdown', 'value')],
+    prevent_initial_call=True
+)
+def update_file_list(selected_bucket):
+    try:
+        if not selected_bucket:
+            return []
+
+        # List objects in the specified bucket
+        objects = s3.list_objects(Bucket=selected_bucket)['Contents']
 
         # Create a list of file information as text and boxes around the files
         file_info = []
         for obj in objects:
             file_path = obj['Key']
-            # Display only files of the first folder level
-            if path and '/' in file_path[len(path):]:
-                continue
             file_size = format_file_size(obj['Size'])
             # Check if the object is a folder or file
             icon = '📂' if file_path.endswith('/') else '📄'
             file_info.append(html.Div([
-                html.Div(icon, style={'marginRight': '5px', 'display': 'inline-block'}),
-                html.Div(f"{file_path}, Size: {file_size}", className='file-box')
+                html.Div(icon, style={'marginRight': '5px', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                html.Div(f"{file_path}, Size: {file_size}", className='file-box', style={'display': 'inline-block', 'vertical-align': 'middle'})
             ], className='file-entry'))
 
     except NoCredentialsError:
-        return "Credentials not available."
+        return []
 
     return file_info
+
+# Function to format file sizes
+def format_file_size(size):
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size < 1024.0:
+            break
+        size /= 1024.0
+    return "{:.2f} {}".format(size, unit)
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8050, debug=True)
